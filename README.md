e0b45873a70e4d42e5d99a72e64c8f4a40d8ac24
Karolina Budzik <47859277+karbudzik@users.noreply.github.com>
2019-05-10 17:24:24 +0200

    The repository of my first commercial project.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Commercial project 1 - Renewable energy provider
+> Website created for the local renewable energy provider. [Available here](http://www.m-power-fotowoltaika.pl)
+
+## Screenshots
+![Website_screenshot](img/screen1.jpg)
+![Website_screenshot_tablet](img/screen2.jpg)
+![Website_screenshot_mobile](img/screen3.jpg)
+
+## Technologies
+* HTML5
+* CSS
+* JavaScript
+
+## Remarks
+The website is based on the very simple structure. I'm aware the CSS code could have been more elegant and I'm missing some SEO elements. However, the goal here was to have fully rosponsive website with as little time spent on creating it as possible.
---
+++ script.js
@@ -0,0 +1,34 @@
+// BURGER MENU:
+
+const burger = document.querySelector(".burgermenu"),
+      navigation = document.querySelector(".mobile-navigation"),
+      property = navigation.style.display;
+
+burger.addEventListener("click", function(){
+    if (window.getComputedStyle(navigation).display == "block") {
+        navigation.style.setProperty("display", "none");
+        burger.style.setProperty("background", "url(img/burgermenu.svg) center no-repeat");
+    } else if (window.getComputedStyle(navigation).display == "none") {
+        navigation.style.setProperty("display", "block");
+        burger.style.setProperty("background", "url(img/burgercross.svg) center no-repeat");
+    }
+});
+
+// IMAGE GALLERY:
+
+const gallery = Array.from(document.querySelectorAll(".gallery")),
+      overlay = document.querySelector(".overlay"),
+      overlayClose = overlay.querySelector(".close"),
+      overlayImage = overlay.querySelector('img');
+
+function handleClick(e) {
+    const src = e.currentTarget.src;
+    overlayImage.src = src;
+    overlay.classList.add("open");
+}
+gallery.forEach(item => item.addEventListener("click", handleClick));
+
+function close() {
+    overlay.classList.remove("open")
+}
+overlayClose.addEventListener("click", close);
